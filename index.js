3213d8c5f903959da75bbe8b2ae3701ee61fc226
iccee0 <66622055+RoeHH@users.noreply.github.com>
2020-09-08 14:48:11 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,11 +2,6 @@ require("dotenv").config()
 const Discord = require("discord.js")
 const fs = require("fs")
 const client = new Discord.Client()
-client.on("message", (msg) => {
-  if (msg.content === "ping") {
-    msg.reply("Pong!")
-  }
-})
 fs.readdir("./events/", (err, files) => {
     files.forEach((file) => {
         const eventHandler = require(`./events/${file}`)
