5ad4999a3a6578504f26170454c48e98cec4b3c8
iccee0 <66622055+RoeHH@users.noreply.github.com>
2020-09-08 15:40:41 +0200

    Update message.js

--- events/message.js
+++ events/message.js
@@ -1,16 +1,13 @@
 const kick = require("../commands/kick")
-try {
     module.exports = (client, message) => {
         if (message.content.startsWith("!kick")) {
             return kick(message)
         }
     }
-} catch {}
+
 const ping = require("../commands/ping")
-try {
     module.exports = (client, message, msg) => {
         if (message.content.startsWith("ping")) {
             return ping(message, msg)
         }
     }
-} catch {}
