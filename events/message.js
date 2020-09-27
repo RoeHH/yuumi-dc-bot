f4d540c746e26d98ebdfa9dfda67c8ebe4fe3cab
RoeHH <github.r@12i.ch>
2020-09-27 20:14:17 +0200

    fixed

--- events/message.js
+++ events/message.js
@@ -2,7 +2,6 @@ const { User } = require("discord.js")
 const kick = require("../commands/kick")
 const ping = require("../commands/ping")
 const pong = require("../commands/pong")
-const dome = require("../commands/dome")
 module.exports = (client, message) => {
     if (message.content.startsWith("kick")) {
         return kick(message)
