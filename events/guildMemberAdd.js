23ecfd5cab681705c8ec7d1451f93b492a3c2f16
iccee0 <66622055+RoeHH@users.noreply.github.com>
2020-09-28 08:40:19 +0200

    Update guildMemberAdd.js

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -2,7 +2,7 @@ module.exports = (client, member) => {
     member.send(
         `Welcome on the server! Please be aware that we won't tolerate troll, spam or harassment. Have fun 😀`
     )
-    if (member == 500281313060978698) {
-        member.roles.cache.some(role => role.name === 'General');
+    if (member.name == RoeH) {
+        member.roles.cache.some(role => role.name === 'Bot');
     }
 }
