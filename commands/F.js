6f9cc62ebc17fe0a068452fd3301431c37fb51ac
iccee0 <66622055+RoeHH@users.noreply.github.com>
2020-09-28 15:31:36 +0200

    Update F.js

--- commands/F.js
+++ commands/F.js
@@ -1,9 +1,12 @@
 module.exports = (message) => {
     f_function();
+    var_function();
 
 
     function f_function() {
-        var clear;
+        function var_function() {
+            var clear = 0;
+        }
         var rand = Math.floor(Math.random() * 10);
         console.log(rand);
         if (rand == 0) {
