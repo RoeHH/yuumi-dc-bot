1ff98b404e6e216a652c9a8ee4b8b60ad0d14eb1
iccee0 <66622055+RoeHH@users.noreply.github.com>
2020-09-28 08:57:30 +0200

    no kick

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -2,7 +2,5 @@ module.exports = (client, member) => {
     member.send(
         `Welcome on the server! Please be aware that we won't tolerate troll, spam or harassment. Have fun 😀`
     )
-    if (member.name == RoeH) {
-        member.roles.cache.some(role => role.name === 'Bot');
-    }
+
 }
--- events/message.js
+++ events/message.js
@@ -1,14 +1,11 @@
 const { User } = require("discord.js")
-const kick = require("../commands/kick.js")
+
 const ping = require("../commands/ping.js")
 const pong = require("../commands/pong.js")
 const avatar = require("../commands/avatar.js")
 const F = require("../commands/F.js")
 
 module.exports = (client, message) => {
-    if (message.content.startsWith("kick")) {
-        return kick(message);
-    }
     if (message.content.startsWith("ping")) {
         return ping(message);
     }
