db60e5c3eab3f20f19ed75521fbc3150be5e93bb
RoeHH <github.r@12i.ch>
2020-11-13 14:35:44 +0100

    fix randomnum

--- commands/randomnumber.js
+++ commands/randomnumber.js
@@ -1,4 +1,8 @@
 module.exports = (message) => {
     var maxSize = message.content.slice(13, 22);
-    message.channel.send(Math.round(Math.random() * maxSize));
+    if (isNaN(message.content)) {
+        message.channel.send("Bitte gebe eine Zahl hinter randomnum ein")
+    } else {
+        message.channel.send(Math.round(Math.random() * maxSize));
+    }
 }
