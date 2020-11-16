e60cf6f4c12cc2e8f0e5266afedfa6e7deb40cc5
iccee0 <66622055+RoeHH@users.noreply.github.com>
2020-11-16 14:07:26 +0100

    Update randomnumber.js

--- commands/randomnumber.js
+++ commands/randomnumber.js
@@ -1,5 +1,5 @@
 module.exports = (message) => {
-    var maxSize = message.content.slice(16, 25);
+    var maxSize = message.content.slice(13, 25);
     if (isNaN(message.content)) {
         message.channel.send("Bitte gebe eine Zahl hinter randomnumber ein")
     } else {
