110b57774b637685a0ba13dd1c4956e75923f0cf
iccee0 <66622055+RoeHH@users.noreply.github.com>
2020-11-16 13:55:02 +0100

    Update opgg.js

--- commands/opgg.js
+++ commands/opgg.js
@@ -1,7 +1,7 @@
 const puppeteer = require('puppeteer');
 
 module.exports = async(message) => {
-    const browser = await puppeteer.launch({ headless: true }{ args: ['--no-sandbox'] });
+    const browser = await puppeteer.launch({ headless: true });
     const page = await browser.newPage();
     await page.setViewport({ width: 1920, height: 2800 });
     if (message.content.slice(3, 4) == "r") {
